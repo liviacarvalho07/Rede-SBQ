1ddf5aab0dc856d31f1ec8a6f23b69cf03baf45c
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2024-05-16 16:36:36 -0300

    Update navbar_cad.php

--- includes_padroes/navbar_cad.php
+++ includes_padroes/navbar_cad.php
@@ -1,7 +1,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="navbar_cad.css">
     <link rel="stylesheet" href="../assents/css/navbar_cad.css">
-
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <!-- Barra de navegação -->
