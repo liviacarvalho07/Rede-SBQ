212ad75af49268f65e15a3cabc5dcbaed8af2eef
Ana Luiza <88158868+Nalu2@users.noreply.github.com>
2024-05-16 14:17:38 -0300

    Add files via upload

---
+++ userfuncional_cadastro.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$grupo_pesquisa = $_POST['grupo_pesquisa'];
+$equipamentos = $_POST['equipamentos'];
+$nome_coordenador = $_POST['nome_coordenador'];
+$area_atuacao = $_POST['area_atuacao '];
+$posicao= $_POST['posicao'];
+$senha = $_POST['senha'];
+$email_coordenador = $_POST['email_coordenados'];
+$confirmar_senha = $_POST['confirmar_senha'];
+$informações_laboratótio = $_POST['informações_laboratótio'];
+$recaptcha = $_POST['recaptcha'];
+
+?>
