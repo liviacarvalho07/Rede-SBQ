0ba7c527d4ad673ccd32ae0dff41b67e63a99293
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2024-05-17 14:33:47 -0300

    Update userfuncional_cadastro.php

--- dependencies/userfuncional_cadastro.php
+++ dependencies/userfuncional_cadastro.php
@@ -18,10 +18,47 @@ if(isset($_POST['submit'])){
       print_r($resposta);
       $resultado = json_decode($resporta);
       if ($resultado-> success == 1){
-        echo "foi";
-        /* Continuacao do codigo */
+      function cadastro_funcional()
+      {
+        global $conn;
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          $nome_funcional = $conn->quote($_POST['nome']);
+          $email_funcional = $conn->quote($_POST['e-mail']);
+          $posicao = $conn->quote($_POST['posicao']);
+          $senha_inserida = $conn->quote($_POST['senha']);
+          $confirmar_senha = $conn->quote($_POST['confirmar_senha']);
+          $area_atuacao = $conn->quote($_POST['area_atuacao']);
+          $grupo_pesquisa = $conn->quote($_POST['grupo_pesquisa']);
+          $nome_coordenador = $conn->quote($_POST['nome_coordenador']);
+          $email_coordenador = $conn->quote($_POST['e-mail_coordenador']);
+          $informacoes_laboratorio = $conn->quote($_POST['informacoes_laboratorio']);
+          $instituicao = $conn->quote($_POST['instituicao']);
+
+          if ($senha_inserida === $confirmar_senha) {
+            $senha_hash = password_hash($senha_inserida, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO cad_funcional (nome_funcional, email_funcional, posicao, senha, area_atuacao, grupo_pesquisa, nome_coordenador, email_coordenador, informacoes_laboratorio, instituicao)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$nome_funcional, $email_funcional, $posicao, $senha_hash, $area_atuacao, $grupo_pesquisa, $nome_coordenador, $email_coordenador, $informacoes_laboratorio, $instituicao]);
+
+            if ($stmt->rowCount() > 0) {
+              header("Location: ../index.php");
+            } else {
+              echo "Error: " . $conn->error;
+            }
+          } else {
+            echo "Senhas não coincidem.";
+          }
+        }
+
+        $conn = null;
       }
 
+      cadastro_funcional();
     }
   }
+}
 ?>
