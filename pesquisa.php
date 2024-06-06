b4072161887250aad580cd4a15c17b24fcfbbf91
Ana Luiza <88158868+Nalu2@users.noreply.github.com>
2024-06-06 14:32:03 -0300

    Create pesquisa.php

---
+++ pesquisa.php
@@ -0,0 +1,41 @@
+<?php include "navbar_index_itens.php"?>
+
+    <!----------- Parte de Pesquisa dos Equipamentos e Laboratórios ----------->
+    <div class="barra">
+      <div class="quadrado">
+        <div class="diretorio">
+            <a href="tela_inicial_pessoal.php"><img src="../itens_navbar_pessoal_index/assets/img/casa.png" alt="Home"></a>
+            > 
+        </div>
+      </div>
+      <div class="triangulo"></div>
+    </div>
+    <div class="pesquisa" style="height: 15rem;">
+        <div class="painel">    
+            <h1 class="convite">Pesquisar novamente?</h1>
+            <div class="digitacao">
+                <input class="caixa" type="text" placeholder="pesquise por laboratórios, equipamentos...">
+                <li><a href="#"> Pesquisar</a></li>
+            </div>
+        </div>
+    </div>
+
+    <!-- Área de catalogo -->
+    <div class="catalogo">
+        <div class="quad_catalogo">
+            <div class="img-equi"></div>
+            <div class="infor-equi">
+                <div class="nome-equip"><h2><b>Nome:</b> Becker</h2></div>
+                <div class="lab-equi"><h4><b>Laboratório:</b> Estadual Sao Lucas</h4></div>
+                <div class="descr-equi"><h6><b>Descrição:</b> sdfsbf gfjksbgk jfsadh vjasdv fljash fdbbhg</h6></div>
+                <div class="mais-detalhes"><a href=""><button>Mais Detalhes</button></a></div>
+            </div>
+        </div>
+    </div>
+    
+    <?php include "../includes_padroes/footer.php"?>
+    <script src="assets/js/script.js"></script>
+</body>
+
+
+</html>
